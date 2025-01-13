6b0d9e8e5648fd044386a3fc84306b04d5e067b6
jorgeferminrl <jorgeferminrl@gmail.com>
2025-01-13 14:28:09 -0700

    05 - Tipos de datos

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+
+var operacion = numero1 + numero2;
+alert("El resultado de la operación es: "+operacion);
+
+// Tipos de datos
+var numeroEntero = 44;              // Numéricos
+var cadenaTexto = 'Hola "qué" tal'; // Textos
+var verdaderoFalso = false;         // Booleano
+var numeroFalso = "33";             // String
+
+
+console.log(cadenaTexto);
+console.log(verdaderoFalso);
+console.log(numeroFalso+7);         // Números en texto se concatenan
+console.log(Number(numeroFalso)+7); // Función Number convierte un número de tipo texto a numérico
+console.log(String(numeroFalso)+4); // Convertir cualquier variable a string
