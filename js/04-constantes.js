4a9bbb0594f1e45ace1ed700835fc45c74abd214
jorgeferminrl <jorgeferminrl@gmail.com>
2025-01-13 09:55:58 -0700

    04 - Constantes

---
+++ js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Constantes
+// Como una variable, pero su valor no puede cambiar
+
+var web = 'https://www.clarilium.com';
+
+const ip = "192.168.0.1";
+
+web = 'www.clarilium.com'
+
+console.log(web, ip);
