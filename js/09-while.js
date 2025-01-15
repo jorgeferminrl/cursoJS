d67842576e16fe845ab77dd85bcc91c23b474d96
jorgeferminrl <jorgeferminrl@gmail.com>
2025-01-15 16:06:17 -0700

    09 - While

---
+++ js/09-while.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var year = 2025;
+
+while (year <= 2051) {
+    // Ejecuta esto
+    console.log("Estamos en el año " + year);
+    // Incremento
+    year++;
+    // Decremento
+    // year--;
+}
