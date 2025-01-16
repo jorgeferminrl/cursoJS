aec2849124f1c5ca8af70a3e1eea814a2ab2594a
jorgeferminrl <jorgeferminrl@gmail.com>
2025-01-15 17:36:40 -0700

    10 - Ventanas

---
+++ js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// ALERTA
+alert("Esta es mi alerta!!");
+alert("Este es mi texto");
+
+// CONFIRMACIÓN
+var miResultado = confirm("¿Estás seguro de continuar?");
+console.log(miResultado);
+
+// INGRESO DATOS
+var edad = prompt("¿Qué edad tienes?", 40);
+console.log(edad);
